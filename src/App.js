665247cee864fa945faab9ce7a6e0869b5d76b01
smitdixit-vai <sdixit@vai.net>
2021-11-03 14:34:49 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+class Developer {
+  
+  constructor(firstName, lastName) {
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+
+  getName() {
+    return this.firstName + " " + this.lastName
+  }
+
+}
+
+const useSemiPersistentState = (key, initialState) => {
+  
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  )
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value)
+  }, [value, key])
+
+  return [value, setValue]
+
+}
+
+const App = () => {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectId: 0
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectId: 1
+    }
+  ]
+
+  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React')
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const searchedStories = stories.filter((story) => 
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+      <h2>React Practice</h2>
+      <hr />
+
+      <InputWithLabel
+        id="search" 
+        value={searchTerm} onInputChange={handleSearch}
+        isFocused="true"
+      >
+        <strong>Search: </strong> 
+      </InputWithLabel>
+      
+      <p>You typed: { searchTerm }</p>
+
+      <List list={searchedStories}/>
+
+    </>
   );
 }
 
-export default App;
+const InputWithLabel = ({id, type="text", value, onInputChange, isFocused, children}) => (
+    <>
+    <label htmlFor={id}>{children}</label>
+    &nbsp;
+    <input type={type} id={id} value={value} onChange={onInputChange} autoFocus={isFocused}></input>
+    </>
+)
+
+const List = ({ list }) => (
+  list.map(({objectId, ...item}) => <Item key={objectId} {...item}/>)
+)
+
+const Item = ({ key, url, title, num_comments, points, author }) => (
+  <div key={key}>
+    <span>
+      <a href={url}>{title}</a>
+    </span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
+  </div>
+)
+
+
+export default App
